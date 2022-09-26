5f974024946f9be2002168379d40e2bf6e0ba39b
ochiryosuke <R>
2022-09-26 23:29:53 +0900

    ボタンでの遷移

---
+++ button/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+//htmlファイルなどの静的ファイル読み込み
+app.use(express.static('public'))
+
+app.get('/',(req,res) =>{
+    res.sendFile(__dirname + '/public/test.html')
+})
+
+app.listen(port,() => {
+    console.log('server start')
+})
