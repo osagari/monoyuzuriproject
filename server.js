0e607d2ac8e10f63f6faba40deda00c07e67170d
ochiryosuke <ht21a023@oecu.jp>
2022-12-23 10:32:59 +0900

    大本のログイン部分をejsを用いて作成

--- server.js
+++ server.js
@@ -3,35 +3,15 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
-const UserRouter = require('./routes/user');
 
-//ミドルウェアの読み込み
-app.use(logger);
+//ログイン用
+const LoginRouter = require('./routes/login');
 
 //ejsテンプレートエンジン設定部分
 app.set('view engine','ejs');
 
+//Loginrouter読み込み
+app.use(LoginRouter);
 
-//localhostのとき
-app.get("/", (request, response) => {
-    //response.send('大元のページです');
-    
-    //ejsを読み込んで表示
-    response.render("index",{text: "Node.jsとexpress"})
-});
-
-//ルーティング部分
-app.use("/user",UserRouter);
-
-//存在しないとき
-app.get('*',(request,response) =>{
-    response.send(404,'お探しのページが見つかりませんでした');
-});
-
-//仲介役のミドルウェア(認証などの管理を行う?)
-function logger(request,response,next){
-    console.log(request.originalUrl);
-    next();
-}
-
+//サーバ起動
 app.listen(port,() => console.log(`サーバが起動しました.ポート番号は${port}です.`));
