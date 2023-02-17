a7a1eb4a7e9250c81079c4a4375fb1f7ea6944d5
BlueFoxVirus <okdkk1myamori@outlook.jp>
2023-02-17 19:14:27 +0900

    modified:   package.json 	new file:   sever.js

---
+++ sever.js
@@ -0,0 +1,45 @@
+//expressの読み込み
+const express = require('express');
+
+const app = express();
+const port = 3000; //ポート番号
+
+//ログイン用
+const LoginRouter = require('./routes/login');
+
+//homeからのルータ
+const HomeRouter = require('./routes/home');
+
+const newRouter = require('./routes/new');
+
+const detailRouter = require('./routes/detail');
+
+const mypageRouter = require('./routes/mypage');
+
+const serachRouter = require('./routes/search');
+
+const chatRouter = require('./routes/chat');
+
+//静的ファイル設定
+app.use(express.static('public'));
+
+//ejsテンプレートエンジン設定部分
+app.set('view engine','ejs');
+
+//Router類読み込み
+app.use(LoginRouter);
+app.use(HomeRouter);
+app.use(newRouter);
+app.use(detailRouter);
+app.use(mypageRouter);
+app.use(serachRouter);
+app.use(chatRouter);
+
+app.get("/",(req,res)=>{
+
+    //localhostにアクセスしたら一旦loginにリダイレクト
+    res.redirect('/login');
+});
+
+//サーバ起動
+app.listen(port,() => console.log(`サーバが起動しました.ポート番号は${port}です.`));
