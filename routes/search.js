d92eeaaa6fc8edfae6fe9d1a840e2e19497a7091
ochiryosuke <ht21a023@oecu.jp>
2023-02-25 11:23:16 +0900

    ポート番号とパラメータ名変更

--- routes/search.js
+++ routes/search.js
@@ -1,33 +1,11 @@
-const express = require('express'); //express 読み込み
-const mysql = require('mysql');
+const express = require('express');
 
 //router
 const router = express.Router();
 
-// mysqlとの接続情報
-/*const dbinfo = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'monoyuzuri_db'
-});
-
-dbinfo.connect((err) =>{
-    if(err) throw err;//接続エラーとなったら例外を出す
-    console.log('database is connectied.'); 
-    let sql = "select student_num from user";//sql文
-
-    //クエリ文で学籍番号を取得
-    dbinfo.query(sql,(err,result,fileds)=>{
-        if(err) throw err;
-        console.log(result); //取得してきたデータはjson形式
-    });
-});*/
-
 // /search
 router.get("/search",(req,res)=>{
-    console.log(req.query.str);
+    console.log(req.query.word); //入力された内容を出力
     res.render("../views/search");
 });
-
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000; //ポート番号
+const port = process.env.PORT || 3000; //ポート番号
 
 //ログイン用
 const LoginRouter = require('./routes/login');
