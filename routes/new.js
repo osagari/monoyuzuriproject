e1804484500d82114efee6bdb57a548a9c6e6099
ochiryosuke <ht21a023@oecu.jp>
2023-03-04 15:59:13 +0900

    競合時に直したつもりになっていた箇所の変更

--- routes/new.js
+++ routes/new.js
@@ -1,36 +1,35 @@
 //express 読み込み
 const express = require('express');
 
+//fileuploadのモジュール読み込み
+const fileupload = require('express-fileupload');
+
 //router
 const router = express.Router();
 
+//使うことを宣言
+router.use(fileupload());
+
 // /new
 router.get("/new",(req,res) => {
-    res.render("../views/new");
+    res.render("../views/new",{imgerror:false});
 });
 
-<<<<<<< HEAD
 //投稿するときのpost(submitのボタンが押されたら呼ばれる)
 router.post("/new", (req,res) =>{
 
-    if(!req.files) return res.status(400).render("../views/new",{imgerror:true});
+    if(!req.files) return res.render("../views/new",{imgerror:true});
 
     let imgFile = req.files.monoimg;
     let imgfilepath = `./uploaded-img/${imgFile.name}`;
-    patharray.push(imgfilepath); //配列に追記する
 
     //画像ファイルの場所指定設定
     imgFile.mv(imgfilepath,(err) =>{
         if(err) {
-            return res.status(500).send(err); //空のレスポンスを返さないためにreturn
+            return res.status(500).send(err);
         }
         res.render("../views/uploadresult");
     });
-=======
-// /new/result
-router.get("/new/result",(req,res)=>{
-    res.render("../views/uploadreult");
->>>>>>> routingbranch
 });
 
 //他のファイルから参照できるようにする
