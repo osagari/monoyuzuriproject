8e5b9c3a8f21755250b33ca384b2d2c546d1066b
BlueFoxVirus <okdkk1myamori@outlook.jp>
2023-04-03 18:56:40 +0900

    search.js、検索実行時、ターミナル、undefind の解決

--- routes/search.js
+++ routes/search.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // /search
 router.get("/search",(req,res)=>{
-    console.log(req.query.word); //入力された内容を出力
+    console.log(req.query.query); //入力された内容を出力
     res.render("../views/search");
 });
 module.exports = router;
