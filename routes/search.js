d9f1c73ccc6ced85d0e10b8b0afc46b0cc87c737
hu-san1008 <gp21a137@oecu.jp>
2023-11-17 00:55:03 +0900

    検索機能の実装

--- routes/search.js
+++ routes/search.js
@@ -1,63 +1,100 @@
+// express 読み込み
 const express = require('express');
-
-//router
 const router = express.Router();
-
 const mysql = require('mysql');
-
-//dotenv読み込み
 const dotenv = require('dotenv');
 const dbinfo = dotenv.config();
 
-
-
-//データベースとの接続プール
-//config()のオブジェクト.parsed -> json形式で記述の意
 const pool = mysql.createPool({
     connectionLimit: dbinfo.parsed.CCL,
     host: dbinfo.parsed.HOSTNAME,
     user: dbinfo.parsed.DBUSER,
     password: dbinfo.parsed.PASSWORD,
     database: dbinfo.parsed.DBNAME,
-    timezone: "jst" ,
+    timezone: "jst",
     charset: "utf8mb4"
 });
+
+async function getimg() {
+    const connection = await new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) reject(err);
+            resolve(connection);
+        });
+    });
+
+    const rows = await new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM img_table', (err, rows) => {
+            if (err) reject(err);
+            resolve(rows);
+        });
+    });
+
+    return rows;
+} // ここに追加
+
+async function searchByKeyword(keyword) {
+    const connection = await new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) reject(err);
+            resolve(connection);
+        });
+    });
+
+    const rows = await new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM img_table`, (err, rows) => {
+            if (err) reject(err);
+            resolve(rows);
+        });
+    });
+
+    const searchData = [];
+
+    for (const row of rows) {
+        const nameData = await getname(row.mono_id);
+        if (nameData[0].mono_name.includes(keyword)) {
+            searchData.push({ ...row, mono_name: nameData[0].mono_name });
+        }
+    }
+
+    return searchData;
+} // ここに追加
+
+async function getname(id) {
+    const connection = await new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) reject(err);
+            resolve(connection);
+        });
+    });
+
+    const rows = await new Promise((resolve, reject) => {
+        connection.query(`SELECT mono_name FROM product_table WHERE mono_id=${id}`, (err, rows) => {
+            if (err) reject(err);
+            resolve(rows);
+        });
+    });
+
+    return rows;
+}
+
 // /search
-router.get("/search",(req,res)=>{
-    console.log(req.query.word);
-    pool.getConnection((err,connection)=>{
-        if(err) throw err; //接続失敗時に例外を出す
-        
-        console.log("接続が完了しました");
+router.get("/search", async (req, res, next) => {
+    try {
+        const keyword = req.query.keyword; // 検索キーワードをクエリパラメータから取得
 
-        const sql='SELECT * FROM img_table';
-        connection.query(sql,(err,rows)=>{
-            connection.release();
-
-            if(err){
-                console.log(err);
-            }
-            console.log(rows[0].img_path);
-            
-        });
-    });
-
-    pool.getConnection((err,connection)=>{
-        if(err) throw err; //接続失敗時に例外を出す
-        
-        console.log("タイトル名の接続が完了しました");
-
-        const sql='SELECT mono_name FROM product_table';
-        connection.query(sql,(err,rows)=>{
-            connection.release();
-
-            if(err){
-                console.log(err);
-            }
-            console.log(rows[0].mono_name);
-            
-        });
-    });
-    res.render("../views/home",{mono_data:newmono_data});
+        if (!keyword) {
+            // キーワードが指定されていない場合は全データを表示
+            const imgData = await getimg();
+            res.render("../views/search", { mono_data: imgData });
+        } else {
+            // キーワードが指定されている場合は該当するデータを表示
+            const searchData = await searchByKeyword(keyword);
+            res.render("../views/search", { mono_data: searchData });
+        }
+    } catch (error) {
+        next(error);
+    }
 });
+
 module.exports = router;
